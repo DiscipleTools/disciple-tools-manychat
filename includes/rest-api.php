6b40c7b82606f6b83433f5eef9e09d68e8ac9cad
Chris Chasm <chris@chasm.solutions>
2019-12-10 17:01:04 -0700

    Updated contact create

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -8,7 +8,7 @@
  */
 class DT_Manychat_Endpoints
 {
-    public $permissions = [ 'view_any_contacts', 'view_project_metrics' ];
+    public $permissions = [ 'create_contacts', 'update_any_contacts' ];
 
     private static $_instance = null;
     public static function instance() {
@@ -52,21 +52,52 @@ class DT_Manychat_Endpoints
 
         $params = $request->get_params();
         set_transient('manychat', $params, '100000' );
-        
+
+        // @todo test for matching API key
+
 //        if ( !$this->has_permission() ){
 //            return new WP_Error( "private_endpoint", "Missing Permissions", [ 'status' => 400 ] );
 //        }
 
+        if ( empty( $params ) ) {
+            return new WP_Error( __METHOD__, "Missing Params", [ 'status' => 400 ] );
+        }
+
         $check_permission = false;
         $fields = [];
 
-        $fields['title'] = 'Test';
+        $fields['title'] = $params['name'] ?? $params['first_name'] ?? $params['key'] ?? 'No ID Supplied By ManyChat';
+        $fields['sources'] = [
+            "values" => [
+                [ "value" => "manychat" ]
+            ]
+        ];
+        if ( isset( $params['phone'] ) && ! empty( $params['phone'] ) ) {
+            $fields['contact_phone'] = [
+                "values" => [
+                    [ "value" => $params['phone'] ]
+                ]
+            ];
+        }
+        if ( isset( $params['email'] ) && ! empty( $params['email'] ) ) {
+            $fields['contact_email'] = [
+                "values" => [
+                    [ "value" => $params['eamil'] ]
+                ]
+            ];
+        }
+        $fields['notes'] = $params['live_chat_url'] ?? '';
 
         $result = Disciple_Tools_Contacts::create_contact( $fields, $check_permission );
 
         if ( is_wp_error( $result ) ) {
             return new WP_Error( 'failed_to_insert_contact', $result->get_error_message() );
         }
+
+        update_post_meta( $result, 'manychat_subscribed', $params['subscribed'] );
+        update_post_meta( $result, 'manychat_key', $params['key'] );
+        update_post_meta( $result, 'manychat_id', $params['id'] );
+        update_post_meta( $result, 'manychat_page_id', $params['page_id'] );
 
         // run your function here
 
