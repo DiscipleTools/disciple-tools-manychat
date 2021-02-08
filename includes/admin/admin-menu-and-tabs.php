0681463064a02143e46b0ad28e647fb5a572f05b
corsacca <corsacca@gmail.com>
2021-02-08 17:50:36 +0100

    Remove duplicate menu declaration: Extensions (DT)

--- includes/admin/admin-menu-and-tabs.php
+++ includes/admin/admin-menu-and-tabs.php
@@ -58,7 +58,6 @@ class DT_Manychat_Menu {
      * @since 0.1
      */
     public function register_menu() {
-        add_menu_page( __( 'Extensions (DT)', 'disciple_tools' ), __( 'Extensions (DT)', 'disciple_tools' ), 'manage_dt', 'dt_extensions', array( $this, 'extensions_menu' ), 'dashicons-admin-generic', 59 );
         add_submenu_page( 'dt_extensions', __( 'ManyChat', 'dt_manychat' ), __( 'ManyChat', 'dt_manychat' ), 'manage_dt', $this->token, array( $this, 'content' ) );
     }
 
